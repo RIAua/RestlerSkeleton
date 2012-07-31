0b40513235402452011a558495733fed8074f649
AGvin <agvinria@gmail.com>
2012-07-31 15:32:36 +0300

    Update modules/say/Say.php

--- modules/say/Say.php
+++ modules/say/Say.php
@@ -4,7 +4,7 @@ class Say {
     // curl -X GET http://<host>/say/hello
     function hello($to = 'world') {
         $ret_data['to'] = $to;
-//        $conf = \Ria\Config::getInstance()->getConfig();
+//        $conf = \Ria\Config::getInstance()->getPreparedConfig();
 //        $db = new Ria\Db\MySQL\MySQLi('main', $conf['db']);
 //        $ret_data['sql_result'] = $db->getArray('SELECT * FROM album');
         
