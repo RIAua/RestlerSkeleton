52b494f1f2058c174d9314b2b4905638668f4a50
AGvin <AGvin@localhost.IT-Sapigura>
2012-09-14 15:52:22 +0300

    update

--- modules/say/Say.php
+++ modules/say/Say.php
@@ -1,13 +1,23 @@
 <?php
+
 //http://restler-skeleton.lcoal-ria.ua/say/hello
 class Say {
+
+    // curl -X GET http://<host>/say/wassup/fred -H="Range: items=0-24"
+    function wassup($word = 'max', $request_range_items = null) {
+        return array(
+            'say'          => $word,
+            'hours_limits' => $request_range_items
+        );
+    }
+
     // curl -X GET http://<host>/say/hello
     function hello($to = 'world') {
         $ret_data['to'] = $to;
 //        $conf = \Ria\Config::getInstance()->getPreparedConfig();
 //        $db = new Ria\Db\MySQL\MySQLi('main', $conf['db']);
 //        $ret_data['sql_result'] = $db->getArray('SELECT * FROM album');
-        
+
         return $ret_data;
     }
 
